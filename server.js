e421190123ee97846d17c8940eea523efb4d4fcd
Adrian <palebluedot85@gmail.com>
2022-03-06 10:53:12 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -2,28 +2,62 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server)
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+const botName = "ChitChat bot";
 //Run when client connects
-io.on("connection", socket => {
-  console.log("new websocket connection");
-  socket.emit('message', 'Welcome to Chitter-Chatter')
-  //Broadcast when a user connects. 
-  //Broadcast emits to all but exept current user
-  socket.broadcast.emit('message', 'A user has joined the chat')
-  //Run when a user disconnects
-  socket.on('disconnect', ()=> {
-      io.emit('message', 'A user has left the chat')
-  })
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    socket.emit("message", formatMessage(botName, "Welcome to ChitChat App"));
+    //Broadcast when a user connects.
+    //Broadcast emits to all but except current user
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+    //Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    })
+
+    //Run when a user disconnects
+    socket.on("disconnect", () => {
+      const user = userLeave(socket.id);
+      if (user) {
+        io.to(user.room).emit(
+          "message",
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+        })
+      }
+    });
+  });
 
   //Listen for chat message
-  socket.on('chatMessage', (msg)=> {
-    io.emit('message', msg)
-  })
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
 });
 
 const PORT = process.env.PORT || 4000;
